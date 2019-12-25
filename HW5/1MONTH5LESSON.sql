9397f3752d4531f5cfc3ad156f973bd061ebb435
Vesert <vesert1@gmail.com>
2019-12-25 20:29:38 +0400

    Bugs fixed and readability improved

--- HW5/1MONTH5LESSON.sql
+++ HW5/1MONTH5LESSON.sql
@@ -1,68 +1,48 @@
 USE WideWorldImporters;
 
 --1. ѕосчитать среднюю цену товара, общую сумму продажи по мес€цам
-SELECT
-          il.Description
-        , MONTH (i.InvoiceDate) AS [month]
-        , AVG (unitprice)       AS avg_price
-        , SUM (quantity)        AS total_sales_qty
-FROM
-          [Sales].[InvoiceLines] il
-          JOIN
-                    [Sales].[Invoices] i
-          ON
-                    il.invoiceid = i.InvoiceID
-GROUP BY
-          il.Description
-        , MONTH (i.InvoiceDate)
+SELECT si.StockItemName,
+       DATEADD(DAY,-DAY(i.InvoiceDate)+1,i.InvoiceDate) AS [month],
+       AVG (il.UnitPrice) AS avg_price,
+       SUM (il.Quantity*il.UnitPrice) AS total_sales_qty
+FROM [Sales].[InvoiceLines] il
+JOIN [Sales].[Invoices] i
+ON il.invoiceid = i.InvoiceID
+JOIN Warehouse.StockItems si
+on il.StockItemID = si.StockItemID
+GROUP BY si.StockItemName, 
+         DATEADD(DAY,-DAY(i.InvoiceDate)+1,i.InvoiceDate)
 ORDER BY
-          il.Description
-        , [month]
+          si.StockItemName, 
+		  [month]
 
 
 -- 2. ќтобразить все мес€цы, где обща€ сумма продаж превысила 10 000 
-SELECT
-          dateadd(day,-day(i.invoicedate)+1,i.InvoiceDate) year_month
-        , SUM (quantity)        AS summa
-FROM
-          [Sales].[InvoiceLines] il
-          JOIN
-                    [Sales].[Invoices] i
-          ON
-                    il.InvoiceId = i.InvoiceId
-GROUP BY
-          dateadd(day,-day(i.invoicedate)+1,i.InvoiceDate)
-HAVING
-          SUM (quantity * UnitPrice) > 10000
-ORDER BY
-          year_month;
+SELECT dateadd(day,-day(i.InvoiceDate)+1,i.InvoiceDate) year_month,
+       SUM (Quantity*UnitPrice)  AS summa
+FROM [Sales].[InvoiceLines] il
+JOIN [Sales].[Invoices] i
+ON il.InvoiceId = i.InvoiceId
+GROUP BY DATEADD(DAY,-DAY(i.InvoiceDate)+1,i.InvoiceDate)
+HAVING SUM(Quantity* UnitPrice) > 10000
+ORDER BY year_month;
 
 
 --3. ¬ывести сумму продаж, дату первой продажи и количество проданного по мес€цам, по товарам, продажи которых менее 50 ед в мес€ц.
-SELECT
-          dateadd(day,-day(i.invoicedate)+1,i.InvoiceDate) year_month
-        , SUM (quantity)        AS total_sale_qty
-        , MIN (i.invoiceDate)   AS min_date
-        , s.stockitemname
-        , COUNT(quantity) AS qty_sale
-FROM
-          [Sales].[InvoiceLines] il
-          JOIN
-                    [Sales].[Invoices] i
-          ON
-                    il.InvoiceId = i.InvoiceId
-          JOIN
-                    [Warehouse].[StockItems] s
-          ON
-                    il.StockItemID = s.StockItemID
-GROUP BY
-          dateadd(day,-day(i.invoicedate)+1,i.InvoiceDate)
-        , s.stockitemname
-HAVING
-          SUM(quantity) < 50
-ORDER BY
-	      year_month
-;
+SELECT dateadd(day,-day(i.invoicedate)+1,i.InvoiceDate) year_month, 
+       SUM (il.Quantity*il.UnitPrice) AS total_sale_qty, 
+	   MIN (i.invoiceDate) AS min_date, 
+	   s.StockItemName, 
+	   SUM(il.Quantity) AS qty_sale
+FROM [Sales].[InvoiceLines] il
+JOIN [Sales].[Invoices] i
+ON il.InvoiceId = i.InvoiceId
+JOIN [Warehouse].[StockItems] s
+ON il.StockItemID = s.StockItemID
+GROUP BY dateadd(day,-day(i.invoicedate)+1,i.InvoiceDate), 
+         s.stockitemname
+HAVING SUM(quantity) < 50
+ORDER BY year_month;
 
 --4. Ќаписать рекурсивный CTE sql запрос и заполнить им временную таблицу и табличную переменную
 WITH directReports (EmployeeID,FirstName,LastName,Title,ManagerID, employeelevel) AS
